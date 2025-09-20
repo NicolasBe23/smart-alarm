9cb3996f5fc495f3a6754ef41d0edf6c0f0fbf5a
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2025-09-21 00:23:11 +0100

    fix: downgrade nativewind to version 2.0.11 and update Tailwind CSS configuration paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
